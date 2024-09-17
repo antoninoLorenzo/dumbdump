59fccb77a65ce718ccb8cee4f90470590bea0dce
Antonino Lorenzo <94693967+antoninoLorenzo@users.noreply.github.com>
2024-09-17 18:58:54 +0200

    Added chromium paths

---
+++ internals/chromium.go
@@ -0,0 +1,19 @@
+package internals
+
+import (
+	"errors"
+)
+
+func getChromiumBasePath(browser string) (string, error) {
+	basePaths := map[string]string{
+		"chrome": "\\AppData\\Local\\Google\\Chrome\\User Data\\",
+		"brave":  "\\AppData\\Local\\BraveSoftware\\Brave-Browser\\User Data",
+	}
+
+	path, ok := basePaths[browser]
+	if ok {
+		return path, nil
+	} else {
+		return string(""), errors.New("Not Found")
+	}
+}
